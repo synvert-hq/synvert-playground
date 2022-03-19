f4370a20004ee0fe11f56dfdcfe738c85bf28666
Richard Huang <flyerhzm@gmail.com>
2022-03-19 19:45:05 +0800

    use monaco-editor

---
+++ src/SourceCodeInput.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import MonacoEditor, { monaco as monacoEditor } from 'react-monaco-editor';
+
+export const SourceCodeInput = () => {
+  const [code, setCode] = useState<string>('// type your code');
+  const options = {
+    selectOnLineNumbers: true
+  };
+
+  const onChange = (value: string, event: monacoEditor.editor.IModelContentChangedEvent) => {
+    setCode(value);
+  }
+
+  const editorDidMount = (editor: monacoEditor.editor.IStandaloneCodeEditor, monaco: typeof monacoEditor) => {
+    editor.focus();
+    console.log(monaco.languages.getLanguages())
+  }
+
+  return (
+    <MonacoEditor
+      width="800"
+      height="600"
+      language="javascript"
+      theme="vs-dark"
+      value={code}
+      options={options}
+      onChange={onChange}
+      editorDidMount={editorDidMount}
+    />
+  );
+}
