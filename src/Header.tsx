21f0dd6c27cd8ab0d70273c079564fec914d1fd0
Richard Huang <flyerhzm@gmail.com>
2022-03-28 17:17:56 +0800

    add logo

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React from "react";
+import logo from "./logo.png";
 
 interface HeaderProps {
   language: string
@@ -10,7 +11,8 @@ interface HeaderProps {
 }
 
 export const Header: React.FC<HeaderProps> = ({ language, languages, handleLanguageChanged, example, examples, handleExampleChanged }) => {
-  const homeUrl = `https://synvert.xinminlabs.com/${language}/home`
+  const homeUrl = `https://synvert.xinminlabs.com/${language}/home`;
+  const offliceSnippetsUrl = `https://synvert.xinminlabs.com/${language}/get_started/`;
 
   const handleLanguageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const language = event.target.value;
@@ -26,9 +28,10 @@ export const Header: React.FC<HeaderProps> = ({ language, languages, handleLangu
     <nav className="bg-neutral-800 text-white shadow">
       <div className="px-5 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div>
-            <a href={homeUrl} target="_blank" rel="noreferrer">Home</a>
-          </div>
+          <a className="flex items-center" href={homeUrl} target="_blank" rel="noreferrer">
+            <img className="w-8 h-8 mr-2" src={logo} alt="Logo" />
+            <span>Home</span>
+          </a>
           <div>
             <span>Languages:&nbsp;&nbsp;</span>
             <select
@@ -40,7 +43,7 @@ export const Header: React.FC<HeaderProps> = ({ language, languages, handleLangu
                 <option key={language}>{language}</option>
               ))}
             </select>
-            <span className="ml-5">Examples:&nbsp;&nbsp;</span>
+            <span className="ml-5">Snippets:&nbsp;&nbsp;</span>
             <select
               value={example}
               className="px-3 py-1.5 text-gray-700 border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:border-blue-600 focus:outline-none"
@@ -50,6 +53,7 @@ export const Header: React.FC<HeaderProps> = ({ language, languages, handleLangu
                 <option key={example}>{example}</option>
               ))}
             </select>
+            <a className="ml-5" href={offliceSnippetsUrl} target="_blank" rel="noreferrer">More Snippets</a>
           </div>
         </div>
       </div>
