51db6e040db5faead5babf1f3fa519a9490e1af8
Richard Huang <flyerhzm@gmail.com>
2022-03-28 16:56:33 +0800

    add Home link

--- src/Header.tsx
+++ src/Header.tsx
@@ -10,6 +10,8 @@ interface HeaderProps {
 }
 
 export const Header: React.FC<HeaderProps> = ({ language, languages, handleLanguageChanged, example, examples, handleExampleChanged }) => {
+  const homeUrl = `https://synvert.xinminlabs.com/${language}/home`
+
   const handleLanguageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const language = event.target.value;
     handleLanguageChanged(language);
@@ -21,29 +23,34 @@ export const Header: React.FC<HeaderProps> = ({ language, languages, handleLangu
   }
 
   return (
-    <nav className="shadow">
+    <nav className="bg-neutral-800 text-white shadow">
       <div className="px-5 lg:px-8">
-        <div className="flex items-center h-16">
-          <span>Languages:&nbsp;&nbsp;</span>
-          <select
-            value={language}
-            className="px-3 py-1.5 text-gray-700 border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:border-blue-600 focus:outline-none"
-            onChange={handleLanguageChange}
-          >
-            {languages.map((language) => (
-              <option key={language}>{language}</option>
-            ))}
-          </select>
-          <span className="ml-5">Examples:&nbsp;&nbsp;</span>
-          <select
-            value={example}
-            className="px-3 py-1.5 text-gray-700 border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:border-blue-600 focus:outline-none"
-            onChange={handleExampleChange}
-          >
-            {examples.map((example) => (
-              <option key={example}>{example}</option>
-            ))}
-          </select>
+        <div className="flex items-center justify-between h-16">
+          <div>
+            <a href={homeUrl} target="_blank" rel="noreferrer">Home</a>
+          </div>
+          <div>
+            <span>Languages:&nbsp;&nbsp;</span>
+            <select
+              value={language}
+              className="px-3 py-1.5 text-gray-700 border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:border-blue-600 focus:outline-none"
+              onChange={handleLanguageChange}
+            >
+              {languages.map((language) => (
+                <option key={language}>{language}</option>
+              ))}
+            </select>
+            <span className="ml-5">Examples:&nbsp;&nbsp;</span>
+            <select
+              value={example}
+              className="px-3 py-1.5 text-gray-700 border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:border-blue-600 focus:outline-none"
+              onChange={handleExampleChange}
+            >
+              {examples.map((example) => (
+                <option key={example}>{example}</option>
+              ))}
+            </select>
+          </div>
         </div>
       </div>
     </nav>
