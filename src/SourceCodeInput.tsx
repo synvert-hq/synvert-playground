6e7a19fcd3c76aa2a8fab17c81230d2a8d6e1a4f
Richard Huang <flyerhzm@gmail.com>
2022-04-04 16:57:08 +0800

    jsx hint

--- src/SourceCodeInput.tsx
+++ src/SourceCodeInput.tsx
@@ -32,7 +32,12 @@ export const SourceCodeInput: React.FC<SourceCodeInputProps> = ({ filePath, setF
       <div className="flex items-center justify-between">
         <span>Source Code:</span>
         <div>
-          <span>File path:</span>
+          <span title={language === 'javascript' ? 'Set file extname as .jsx if source code contains any jsx element.' : ''}>
+            File path:
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 inline" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
+            </svg>
+          </span>
           <input
             className="w-60 border border-gray-300 text-gray-900 mx-2 px-2 rounded"
             type="text"
