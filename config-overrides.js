72da4f006c1d3b28d079cbef4c6c07dec6ed0dcd
Awesome Code <team@xinminlabs.com>
2022-04-16 09:52:49 +0000

    Auto corrected by following Lint Typescript unicorn/prevent-abbreviations

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,6 @@
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
-module.exports = function override(config, env) {
+module.exports = function override(config, environment) {
   config.plugins.push(
     new MonacoWebpackPlugin({
       languages: ["javascript", "ruby"],
