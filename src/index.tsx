0d605d1ddefedfc03ff42ee0a235f2085547edec
Richard Huang <flyerhzm@gmail.com>
2022-08-13 20:28:06 +0800

    add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route path=":language">
+            <Route path="parse-snippet" element={<App />} />
+            <Route path="generate-snippet" />
+            <Route path="generate-ast" />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
