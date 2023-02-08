25e02d6a28675bad4d15b27b80589fbc61bda401
Richard Huang <flyerhzm@gmail.com>
2023-02-08 17:08:30 +0800

    support light / dark mode

--- src/shared/TextField.tsx
+++ src/shared/TextField.tsx
@@ -19,7 +19,7 @@ const TextField: React.FC<TextFieldProps> = ({
   return (
     <input
       type="text"
-      className="bg-gray-800 border border-gray-500 text-gray-50 text-sm rounded-lg focus:ring-gray-200 focus:border-gray-100 block w-full p-2.5"
+      className="bg-gray-100 dark:bg-gray-800 border dark:border-gray-500 text-gray-900 dark:text-gray-50 text-sm rounded-lg focus:ring-gray-200 focus:border-gray-100 block w-full p-2.5"
       value={value}
       placeholder={placeholder}
       onChange={onChange}
