d8b9c1e43bd519939c18c982672bd1a6bf403965
Richard Huang <flyerhzm@gmail.com>
2024-01-12 08:48:26 +0800

    better error message

--- src/Alert.tsx
+++ src/Alert.tsx
@@ -13,7 +13,7 @@ const Alert: React.FC = () => {
 
   return (
     <div className="bg-red-100 border border-red-400 text-red-700 mx-4 my-3 px-4 py-3 rounded relative">
-      <span className="block sm:inline">{alert}</span>
+      <span className="block sm:inline">{alert}<br/><br/>If it is our fault, we will see the error and try to fix it ASAP.</span>
       <button
         className="absolute top-0 bottom-0 right-0 px-4 py-3"
         onClick={onClose}
