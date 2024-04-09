f4a08b3951b08ed2eefb6e6cf0477ed672fd43ee
Richard Huang <flyerhzm@gmail.com>
2024-04-09 13:24:17 +0800

    update footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -2,10 +2,10 @@ const Footer = () => (
   <footer className="mt-8 px-2 py-4 border-t border-gray-500 text-center">
     <p>
       ©{" "}
-      <a href="https://xinminlabs.com" target="_blank" rel="noreferrer">
-        Xinmin Labs
+      <a href="https://synvert.net" target="_blank" rel="noreferrer">
+        synvert.net
       </a>{" "}
-      2023. All rights reserved.
+      2024. All rights reserved.
     </p>
   </footer>
 );
